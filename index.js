d7f8c7e6013fb75be09550e767e131a8f61a8c2f
Als <malkautsars@gmail.com>
2020-04-10 22:13:21 +0800

    feat: include swagger.json

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 app.get('/', (req, res) => {
-    res.send("Hello Word")
+    res.send("direct to /api-docs to see the documentation")
 });
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3000, () => {
     console.log('Server running at port 3000: http://127.0.0.1:3000')
